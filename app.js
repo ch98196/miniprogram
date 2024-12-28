82d9c5f70a7ad4ddbf36f6fe57ddebb30b69f661
ch98196 <chjd9896@163.com>
2024-12-28 11:19:14 +0800

    commit

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 // app.js
 App({
   onLaunch() {
-
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+      if (res.hasUpdate) {
+        console.log('发现新版本')
+      }
+    })
+    updateManager.onUpdateReady(() => {
+      updateManager.applyUpdate()
+    })
   },
   onThemeChange(e){
     wx.setNavigationBarColor({
